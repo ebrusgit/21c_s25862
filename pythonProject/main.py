dedecf28e7d7747bd8aace0bfe39aa3e125013f1
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-03-21 17:03:16 +0100

    task 2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,3 +1,13 @@
 #TASK 1
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+#TASK 2
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
+
+start = 1
+end = 10
+squares_list = generate_squares(start, end)
+print(squares_list)
