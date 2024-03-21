916354a0f251eed00cc49933293bdc5a578c41b6
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-03-21 17:06:05 +0100

    task 5

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -38,3 +38,28 @@ start = 1
 end = 10
 square_roots_list = square_gen.generate_squares(start, end)
 print(square_roots_list)
+
+#TASK 5
+import math
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("Error: end cannot be less than the start")
+        squares = [x**2 for x in range(start, end + 1)]
+        square_roots = [int(math.sqrt(num)) if math.sqrt(num).is_integer() else math.sqrt(num) for num in squares]
+        return square_roots
+
+square_gen = SquareGenerator()
+start = 1
+end = 10
+square_roots_list = square_gen.generate_squares(start, end)
+print(square_roots_list)
+
+start = 10
+end = 1
+try:
+    square_roots_list = square_gen.generate_squares(start, end)
+    print(square_roots_list)
+except ValueError as e:
+    print(e)
