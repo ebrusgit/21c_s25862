05e394a7b65244d2acbf49ac5128e7da139e047e
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-03-21 17:04:16 +0100

    task 3

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -11,3 +11,16 @@ start = 1
 end = 10
 squares_list = generate_squares(start, end)
 print(squares_list)
+
+
+#TASK 3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
+
+square_gen = SquareGenerator()
+start = 1
+end = 10
+squares_list = square_gen.generate_squares(start, end)
+print(squares_list)
