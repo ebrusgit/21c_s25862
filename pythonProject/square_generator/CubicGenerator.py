6263b13a42f816267a2bcea19964cbf13c422100
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-03-21 17:21:53 +0100

    task 9

--- pythonProject/square_generator/CubicGenerator.py
+++ pythonProject/square_generator/CubicGenerator.py
@@ -11,3 +11,32 @@ start = 1
 end = 5
 cubes_list = cubic_gen.generate_squares(start, end)
 print("Cubes:", cubes_list)
+
+
+#TASK 9
+from square_generator import SquareGenerator
+class CubicGenerator(SquareGenerator):
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("Error: end cannot be less than the start")
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
+
+cubic_gen = CubicGenerator()
+start = 1
+end = 5
+try:
+    squares_list = cubic_gen.generate_squares(start, end)
+    print("Squares:", squares_list)
+except ValueError as e:
+    print(e)
+
+#TASK 9 checker for the error
+print("task 9 checker:")
+start = 5
+end = 1
+try:
+    squares_list = cubic_gen.generate_squares(start, end)
+    print("Squares:", squares_list)
+except ValueError as e:
+    print(e)
